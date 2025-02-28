7814fefa835029678bf9e8847d4cedfe19d30ce4
Abhishek Sinha <abhisheksinha@Abhisheks-MacBook-Air.local>
2025-02-28 16:14:44 -0500

    modified the code

--- memory_app.py
+++ memory_app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 
-# # Custom CSS for Background Image
-# st.markdown(
-#     """
-#     <style>
-#     .stApp {
-#         background-image: url("background.jpeg");
-#         background-size: cover;
-#         background-position: center;
-#         background-repeat: no-repeat;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
+# Custom CSS for Background Image
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("background.jpeg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Header Text
 st.markdown(
-    "<h1 style='text-align: center; color: white;'>Memory App</h1>", 
+    "<h1 style='text-align: center; color: pink;'>Memory App</h1>", 
     unsafe_allow_html=True
 )
