866afcd00cb5b69245168b6a0ce49c7f7694ec20
Abhishek Sinha <abhisheksinha@Abhisheks-MacBook-Air.local>
2025-02-28 12:48:57 -0500

    changed the app name using CSS

--- memory_app.py
+++ memory_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.title("This ia a memory app of Abhi and Ven")
+st.markdown(
+    "<h1 style='text-align: center; color: red; font-size: 36px; font-weight: bold;'>This is a memory app of Abhi and Ven</h1>", 
+    unsafe_allow_html=True
+)
