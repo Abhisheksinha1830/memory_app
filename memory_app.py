deba8c9c3cb26cf878c557d88fd8c4f1fff0dfd2
Abhishek Sinha <abhisheksinha@Abhisheks-MacBook-Air.local>
2025-03-07 20:08:17 -0500

    added few codes

--- memory_app.py
+++ memory_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image  # Changed 'image' to 'Image'
 
-img1 = Image.open("/Users/abhisheksinha/Desktop/Ven/surprise/sad_puppy.jpg")
-img2 = Image.open("/Users/abhisheksinha/Desktop/Ven/surprise/heart.jpg")
-img3 = Image.open("/Users/abhisheksinha/Desktop/Ven/surprise/flowers.jpg")
-img4 = Image.open("/Users/abhisheksinha/Desktop/Ven/surprise/special_photo.jpeg")
+img1 = Image.open("sad_puppy.jpg")
+img2 = Image.open("heart.jpg")
+img3 = Image.open("flowers.jpg")
+img4 = Image.open("special_photo.jpeg")
 
 # st.image(Image.open("background.jpeg"))
 
